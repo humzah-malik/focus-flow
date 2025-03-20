0f66dfa46d680aee0807d2a7eee357062a89afe2
humzah-malik <humzahmalik04@gmail.com>
2025-03-20 17:59:33 -0400

    Update passport.js

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -3,8 +3,7 @@ const passport = require('passport');
 const OAuth2Strategy = require('passport-oauth2');
 const axios = require('axios');
 const path = require('path');
-// near the top of passport.js
-const User = require('../models/user');  // <--- Make sure you have this!
+const User = require('../models/user'); 
 const LocalStrategy = require('passport-local').Strategy;
 require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
@@ -16,49 +15,6 @@ if (process.env.NODE_ENV === 'development') {
     redirectURI: process.env.TODOIST_REDIRECT_URI,
   });
 }
-
-/*
-// Configure Todoist OAuth2 strategy
-passport.use('todoist', new OAuth2Strategy(
-  {
-    authorizationURL: 'https://todoist.com/oauth/authorize',
-    tokenURL: 'https://todoist.com/oauth/access_token',
-    clientID: process.env.TODOIST_CLIENT_ID,
-    clientSecret: process.env.TODOIST_CLIENT_SECRET,
-    callbackURL: process.env.TODOIST_REDIRECT_URI,
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      // Fetch user info from Todoist's sync API
-      const response = await axios.get('https://api.todoist.com/sync/v9/sync', {
-        params: {
-          sync_token: '*',
-          resource_types: '["user"]',
-        },
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-
-      const userInfo = response.data.user;
-
-      // Build the user object
-      const user = {
-        accessToken,
-        refreshToken,
-        id: userInfo.id,
-        name: userInfo.full_name,
-        email: userInfo.email,
-      };
-
-      return done(null, user);
-    } catch (error) {
-      console.error('Error fetching Todoist user info:', error);
-      return done(error);
-    }
-  }
-));
-*/
 
 passport.use(
   new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
